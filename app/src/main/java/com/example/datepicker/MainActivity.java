aed21b2ad7105074529d2fc0388507d343d351dc
seema kale <seemakale216@gmail.coma>
2019-09-12 00:57:43 -0700

    Initial commit

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.editText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar c = Calendar.getInstance();
+                int mYear=c.get(Calendar.YEAR);
+                int mMonth=c.get(Calendar.MONTH);
+                int mDay=c.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog=new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        editText.setText(i2+"/"+(i1+1)+"/"+i);
+
+                    }
+                },mYear,mMonth,mDay);
+                datePickerDialog.show();
+
+            }
+        });
+    }
+}
